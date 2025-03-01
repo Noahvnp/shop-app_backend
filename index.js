d4f319d657618930b0dfad825048212171967a79
Noahvnp <norahpvn@gmail.com>
2025-03-01 21:49:07 +0700

    feat: setup express with nodemon, dotenv, and babel

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("This is my shop-app backend!");
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.get("/", (req, res) => {
+  res.send("Shop app is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
