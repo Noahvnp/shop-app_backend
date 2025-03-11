4c91068c77733433904731acdb207bd5668c1a01
Noahvnp <norahpvn@gmail.com>
2025-03-11 13:35:50 +0700

    feat: add validation middleware to product routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,12 +1,20 @@
 import express from "express";
 import * as productControllers from "../controllers/productControllers.js";
+import { validate } from "../middlewares/validate.js";
+import { idSchema } from "../validators/commonValidation.js";
+import { productSchema } from "../validators/productValidaton.js";
 
 const router = express.Router();
 
 router.get("/", productControllers.getProducts);
-router.get("/:id", productControllers.getProductById);
-router.post("/", productControllers.createProduct);
-router.put("/:id", productControllers.updateProduct);
-router.delete("/:id", productControllers.deleteProduct);
+router.get("/:id", validate(idSchema, "params"), productControllers.getProductById);
+router.post("/", validate(productSchema), productControllers.createProduct);
+router.put(
+  "/:id",
+  validate(idSchema, "params"),
+  validate(productSchema),
+  productControllers.updateProduct,
+);
+router.delete("/:id", validate(idSchema, "params"), productControllers.deleteProduct);
 
 export default router;
